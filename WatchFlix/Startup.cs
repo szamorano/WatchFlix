c8ed6c2ecd78a15c3572650dbc2e4977a664f1a9
Steven Zamorano <stevenzamorano.code@gmail.com>
2018-01-12 13:33:08 -0500

    Add project files.

---
+++ WatchFlix/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WatchFlix.Startup))]
+namespace WatchFlix
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
