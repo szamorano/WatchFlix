2a48d0448bd2343130e485179c9664011bd7015d
Steven Zamorano <stevenzamorano.code@gmail.com>
2018-01-14 12:01:19 -0500

    Added the Customers and Movies Controllers
    Along with their corresponding Razor views.

--- WatchFlix/Controllers/MoviesController.cs
+++ WatchFlix/Controllers/MoviesController.cs
@@ -4,24 +4,45 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using WatchFlix.Models;
+using WatchFlix.ViewModels;
 
 namespace WatchFlix.Controllers
 {
     public class MoviesController : Controller
     {
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek" },
+                new Movie { Id = 2, Name = "Wall-e" }
+            };
+        }
+
         // GET: Movies/Random
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek!" };
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "Customer 1" },
+                new Customer { Name = "Customer 2" }
+            };
 
-            return View(movie);
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
 
-        }
-
-        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1, 12)}")]
-        public ActionResult ByReleaseYear(int year, int month)
-        {
-            return Content(year + "/" + month);
+            return View(viewModel);
         }
     }
 }
---
+++ WatchFlix/ViewModels/RandomMovieViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WatchFlix.Models;
+
+namespace WatchFlix.ViewModels
+{
+    public class RandomMovieViewModel
+    {
+        public Movie Movie { get; set; }
+        public List<Customer> Customers { get; set; }
+    }
+}
