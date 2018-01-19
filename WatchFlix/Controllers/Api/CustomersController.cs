c5bd4ba8d1ccfe068e1553f7b5fcec7b53e5531a
Steven Zamorano <stevenzamorano.code@gmail.com>
2018-01-19 14:14:19 -0500

    Built ASP.NET Web API

---
+++ WatchFlix/Controllers/Api/CustomersController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WatchFlix.Models;
+
+namespace WatchFlix.Controllers.Api
+{
+    public class CustomersController : ApiController
+    {
+        private ApplicationDbContext db;
+
+        public CustomersController()
+        {
+            db = new ApplicationDbContext();
+        }
+
+        // GET /api/customers
+        public IEnumerable<Customer> GetCustomers()
+        {
+            return db.Customers.ToList();
+        }
+
+        // GET /api/customers/1
+        public Customer GetCustomer(int id)
+        {
+            var customer = db.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            return customer;
+        }
+
+        // POST /api/customers
+        [HttpPost]
+        public Customer CreateCustomer(Customer customer)
+        {
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            db.Customers.Add(customer);
+            db.SaveChanges();
+
+            return customer;
+        }
+
+        // PUT /api/customers/1
+        [HttpPut]
+        public void UpdateCustomer(int id, Customer customer)
+        {
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            var customerInDb = db.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customerInDb == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            customerInDb.Name = customer.Name;
+            customerInDb.Birthdate = customer.Birthdate;
+            customerInDb.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter;
+            customerInDb.MembershipTypeId = customer.MembershipTypeId;
+
+            db.SaveChanges();
+
+        }
+
+        // DELETE /api/customers/1
+        public void DeleteCustomer(int id)
+        {
+            var customerInDb = db.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customerInDb == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            db.Customers.Remove(customerInDb);
+        }
+
+    }
+}
