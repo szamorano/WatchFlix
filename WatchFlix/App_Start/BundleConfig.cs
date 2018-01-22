619769101704faea77bf8804acbab16f54441b24
Steven Zamorano <stevenzamorano.code@gmail.com>
2018-01-22 13:15:30 -0500

    Implemented BootBox

--- WatchFlix/App_Start/BundleConfig.cs
+++ WatchFlix/App_Start/BundleConfig.cs
@@ -21,6 +21,7 @@ namespace WatchFlix
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
+                      "~/scripts/bootbox.js",
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
